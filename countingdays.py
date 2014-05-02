24ee7dd5a556c1a78711d02dc5233facb9b9a7b1
cactapus <satvikliving@gmail.com>
2014-05-02 16:49:05 -0700

    pythonroom update

--- countingdays.py
+++ countingdays.py
@@ -10,7 +10,8 @@ for year in years:
 		days = range(1, 32)
 		if month == 4 or month ==6 or month == 9 or month == 11:
 			days = range(1, 31)
-			
+		if month == 2:
+			days = range(1,29)
 		
 		
 		for day in days:
