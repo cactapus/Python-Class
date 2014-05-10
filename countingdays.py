e97d82fdb38f55f11f7d1c975212344169ba61ad
cactapus <satvikliving@gmail.com>
2014-05-09 17:39:11 -0700

    pythonroom update

---
+++ countingdays.py
@@ -0,0 +1,18 @@
+answer = input("Speak,human.")
+answer = answer.lower()
+word = answer.split()
+
+if word[0] == "will":
+	if word[1] == "you":
+		if word[2] == "marry":
+			if word[3] == "me?":
+				print "no"
+if word[0] == "why?":
+	print "you are ugly."
+
+if answer == "that's okay, i already married your sister!":
+	print "Martha steals everything from me"
+		
+
+	
+			
